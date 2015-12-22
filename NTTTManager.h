e7b8b0a07c0fcf70296c332986a8053c5215bc01
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2015-12-22 10:05:53 +0100

    More refactoring

--- NTTTManager.h
+++ NTTTManager.h
@@ -23,20 +23,30 @@ class NTTTManager
 		void writeLog();
 
 
+    // These variables are accessed from OUTSIDE the NTTTManager class via the
+    // global variable g_NtttManager
+    // TODO: Put these variables in a separate class
+    public:
+        SDL_Renderer* g_renderer = NULL;		//Pointer pointing to a struct representing the renderer
+        Texture *g_redCross;
+        Texture *g_blueCross;
+        Texture *g_checkMark;
+        const int PADDING_X = 3, PADDING_Y = 5;	//The padding along the axises
+        int g_textHeight = 0;					//The 'maximum' text height used with the font
+        TTF_Font* g_font = NULL;				//Pointer pointing to the representation of the font
+
+
+    // These variables are only used WITHIN the NTTTManager class
+    private:
         const char* TITLE = "No Tic Tac Toe";			//The title of the window
         const int WINDOW_WIDTH = 800;					//The width of the window
         const int WINDOW_HEIGHT = 600;					//The height of the window
         const int FONT_SIZE = 30;						//The size of the font
         const char* FONT_PATH = "Junicode-Regular.ttf";	//The path to the font
-        const int PADDING_X = 3, PADDING_Y = 5;	//The padding along the axises
         const int BOARD_PADDING = 10;
 
         SDL_Window* g_window = NULL;			//Pointer pointing to a struct representing the window
-        SDL_Renderer* g_renderer = NULL;		//Pointer pointing to a struct representing the renderer
-        TTF_Font* g_font = NULL;				//Pointer pointing to the representation of the font
-        int g_textHeight = 0;					//The 'maximum' text height used with the font
 
-        Texture *g_redCross, *g_blueCross, *g_checkMark;
         NTTTGame *g_game;
 
 		Text *boardCountText = nullptr, *boardSizeText = nullptr,
