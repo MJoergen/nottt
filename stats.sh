e9ab19150bb52372d1945a15b707a79f91834d95
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2016-02-03 13:01:07 +0100

    Improved output.

--- stats.sh
+++ stats.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 rm log-*.txt
-time nottt 1 3 3 100 log-1-3-3.txt | tail -n 8
-time nottt 2 3 3 100 log-2-3-3.txt | tail -n 8
-time nottt 3 3 3 100 log-3-3-3.txt | tail -n 8
-time nottt 1 4 3 100 log-1-4-3.txt | tail -n 8
-time nottt 2 4 3 100 log-2-4-3.txt | tail -n 8
-time nottt 3 4 3 100 log-3-4-3.txt | tail -n 8
-time nottt 1 5 3 100 log-1-5-3.txt | tail -n 8
-time nottt 2 5 3 100 log-2-5-3.txt | tail -n 8
-time nottt 3 5 3 100 log-3-5-3.txt | tail -n 8
-time nottt 1 6 3 100 log-1-6-3.txt | tail -n 8
-time nottt 2 6 3 100 log-2-6-3.txt | tail -n 8
-time nottt 3 6 3 100 log-3-6-3.txt | tail -n 8
+time nottt 1 3 3 100 log-1-3-3.txt | tail -n 9
+time nottt 2 3 3 100 log-2-3-3.txt | tail -n 9
+time nottt 3 3 3 100 log-3-3-3.txt | tail -n 9
+time nottt 1 4 3 100 log-1-4-3.txt | tail -n 9
+time nottt 2 4 3 100 log-2-4-3.txt | tail -n 9
+time nottt 3 4 3 100 log-3-4-3.txt | tail -n 9
+time nottt 1 5 3 100 log-1-5-3.txt | tail -n 9
+time nottt 2 5 3 100 log-2-5-3.txt | tail -n 9
+time nottt 3 5 3 100 log-3-5-3.txt | tail -n 9
+time nottt 1 6 3 100 log-1-6-3.txt | tail -n 9
+time nottt 2 6 3 100 log-2-6-3.txt | tail -n 9
+time nottt 3 6 3 100 log-3-6-3.txt | tail -n 9
